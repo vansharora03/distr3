8f65f3cf1e1560d54c36215a29439a9cba74f396
Vansh Arora <vansh94arora@gmail.com>
2025-03-24 17:16:36 -0400

    done

--- Coordinator.java
+++ Coordinator.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 public class Coordinator {
     public int port;
     public int timeThreshold;
     public Message[] messages;
+    public HashMap<String, Main> participants;S
     
     public static void main(String[] args) {
         if (args.length != 1) {
