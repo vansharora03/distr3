7ce58ab2ae4228c5d046572b45c74f7722f2da32
dhruv2253 <dhruv225@outlook.com>
2025-03-24 23:22:20 -0400

    Layout

--- Main.java
+++ Main.java
@@ -1,13 +1,32 @@
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
+    String coordinatorIP;
+    int coordinatorPort;
 
     public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("127.0.0.1", 1025);
-        } catch (Exception e) {
-            System.out.println("Error connecting to coordinator");
-            System.exit(1);
+        String coordinatorIP = args[0];
+        int coordinatorPort = Integer.parseInt(args[1]);
+    }
+
+    public void handleCommand() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            sendToCoordinator(command);
         }
     }
-}   
+
+    private void sendToCoordinator(String command) {
+        try {
+            Socket socket = new Socket(coordinatorIP, coordinatorPort);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
