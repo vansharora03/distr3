e905dde80d0a888b7b77fffb82c603e3ebc1760b
dhruv2253 <dhruv225@outlook.com>
2025-03-25 13:37:19 -0400

    Connect to coordinator

--- Main.java
+++ Main.java
@@ -6,29 +6,51 @@ public class Main {
     // TODO: Make variables for all the parameters in the config file
     String coordinatorIP;
     int coordinatorPort;
+    int id;
+    String messageFile;
 
-    public static void main(String[] args) {
-        String coordinatorIP = args[0];
-        int coordinatorPort = Integer.parseInt(args[1]);
-        // TODO: Read from config file to find necessary info instead of args ^
+    public Main(String configFile) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(configFile));
+        this.id = Integer.parseInt(reader.readLine().trim());
+        this.messageFile = reader.readLine().trim();
+        String[] line = reader.readLine().trim().split(" ");
+        this.coordinatorIP = line[0];
+        this.coordinatorPort = Integer.parseInt(line[1]);
     }
 
-    public void handleCommand() {
+    public void start() {
+        new Thread().start();
+        System.out.println("Started");
+        handleCommands();
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java main <config file name>");
+            System.exit(1);
+        }
+        try {
+            Main main = new Main(args[0]);
+            Socket socket = new Socket(main.coordinatorIP, main.coordinatorPort);
+            System.out.println("Server started on " + main.coordinatorIP + " at port " + main.coordinatorPort);
+            main.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void handleCommands() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            System.out.print("> ");
             String command = scanner.nextLine();
             sendToCoordinator(command);
         }
     }
 
     private void sendToCoordinator(String command) {
-        try {
-            Socket socket = new Socket(coordinatorIP, coordinatorPort);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            out.println(command);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return;
     }
 
     // TODO: Log messages to a file
