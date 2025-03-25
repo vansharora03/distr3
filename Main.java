bd389d4a21c4f36d2914a1b41baa95223dd60b26
dhruv2253 <dhruv225@outlook.com>
2025-03-24 23:28:29 -0400

    add todo

--- Main.java
+++ Main.java
@@ -3,12 +3,14 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
+    // TODO: Make variables for all the parameters in the config file
     String coordinatorIP;
     int coordinatorPort;
 
     public static void main(String[] args) {
         String coordinatorIP = args[0];
         int coordinatorPort = Integer.parseInt(args[1]);
+        // TODO: Read from config file to find necessary info instead of args ^
     }
 
     public void handleCommand() {
@@ -29,4 +31,13 @@ public class Main {
         }
     }
 
+    // TODO: Log messages to a file
+    public void logMessages(String message) {
+        return;
+    }
+
+    // TODO: Listen for message
+    public void listenMessage() {
+        return;
+    }
 }
